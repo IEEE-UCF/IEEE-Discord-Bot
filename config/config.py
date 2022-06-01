e3762829e1445726c398cdee051c27e49508f45f
Edward Weir <ejweir0001@gmail.com>
2022-06-01 16:44:01 -0400

    cosutom config for reaction roles

---
+++ config/config.py
@@ -0,0 +1,19 @@
+"""
+Once a role has been made on the discord server 
+then you can assign the emjoi with the roles name here 
+
+Example Template:
+
+reaction_roles = {
+    'emoji_1':'Role_1',
+    'emoji_2':'Role_2'
+}
+
+"""
+
+reaction_roles = {
+    '🔥':'Test Role'
+}
+
+#channel ID of where the emebed will be sent that can be reacted on to add roles
+channelID = 974865668262477865
